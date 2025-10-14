4e46f2904518786a99d6da77c7a94c9c80da5d8b
Fernando González <101148750+Joacojgn@users.noreply.github.com>
2025-10-14 13:56:16 -0300

    codigo actualizado para probar un tx y un rx

--- esp32/main.py
+++ esp32/main.py
@@ -3,12 +3,8 @@ from machine import Pin
 import time
 
 
-RX_PIN = 21  
-TX_PIN = 20 
-
-
-rx_via = Rx(RX_PIN)
-tx_locomotora = Tx(TX_PIN)  
+rx_via = 21        
+tx_locomotora = 20  
 
 
 ESTADOS = {
@@ -17,29 +13,38 @@ ESTADOS = {
     2: 'OCUPADO'
 }
 
+NOMBRE_LOCOMOTORA = "PROTORP"
+ID_LOCOMOTORA = 7
 
-VELOCIDAD = 3  
-ID_LOCO = 7    
+
 
 def recibir_estado():
     try:
         valor = rx_via.listen()
         estado = ESTADOS.get(valor, 'DESCONOCIDO')
-        print(f"Estado recibido: {estado} ({valor})")
+        print("PROTORP recibió estado:", estado, f"({valor})")
         return estado
     except:
         print("Error de recepción")
         return None
 
-def transmitir_respuesta():
-    
-    respuesta = (VELOCIDAD << 4) | ID_LOCO  
-    tx_locomotora.send_byte(respuesta)
-    print(f"Respuesta enviada: velocidad={VELOCIDAD}, ID={ID_LOCO}, byte={respuesta}")
+def transmitir_respuesta(estado):
+    if estado == 'LIBRE':
+        velocidad_kmh = 120
+    elif estado == 'PRECAUCION':
+        velocidad_kmh = 50
+    elif estado == 'OCUPADO':
+        velocidad_kmh = 0
+    else:
+        velocidad_kmh = 0
 
+    velocidad_bits = min(int(velocidad_kmh / 8), 15)  
+    respuesta = (velocidad_bits << 4) | 7             
+    tx_protorp.send_byte(respuesta)
+    print(f"PROTORP,Velocidad={velocidad_kmh} km/h, ID=7, Byte={respuesta}")
 
 while True:
     estado = recibir_estado()
-    if estado:
-        transmitir_respuesta()  
+    if estado in ESTADOS.values():
+        transmitir_respuesta(estado)
     time.sleep(1)
