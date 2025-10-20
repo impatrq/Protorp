f2e0d61b2310d90ee71c12a2a3e7181a9e578c08
Fernando González <101148750+Joacojgn@users.noreply.github.com>
2025-10-20 14:31:55 -0300

    actualizo codigo para incluir puente PWM y Bluetooth
    actualizo el codigo del esp32 para incluir el control de motor con el driver L298N  y control remoto con Bluetooth

--- esp32/main.py
+++ esp32/main.py
@@ -1,37 +1,64 @@
+from machine import Pin, PWM
+import time, random, bluetooth
+from ble_simple_peripheral import BLESimplePeripheral
 from rf_simple import Tx, Rx
-from machine import Pin
-import time
-import random
 
-tx_locomotora = Tx(20)
-rx_estado = Rx(21)
 
-VEL_MAXIMA = 120
+ENA = PWM(Pin(4), freq=1000)   
+IN1 = Pin(3, Pin.OUT)          
+IN2 = Pin(2, Pin.OUT)          
+
+
+tx = Tx(20)  
+rx = Rx(21)  
+
+
+ble = bluetooth.BLE()
+sp = BLESimplePeripheral(ble)
+
+VEL_MAX = 120
 ID_LOCOMOTORA = 7
-NOMBRE_LOCOMOTORA = "PROTORP"
+NOMBRE = "PROTORP"
+
+def set_motor_speed(vel):
+    duty = int(min(max(vel, 0), VEL_MAX) / VEL_MAX * 1023)
+    ENA.duty(duty)
+    if vel > 0:
+        IN1.value(1)
+        IN2.value(0)
+    else:
+        IN1.value(0)
+        IN2.value(0)
 
 def recibir_estado():
     try:
-        valor = rx_estado.listen()
+        valor = rx.listen()
         estados = {0: 'LIBRE', 1: 'PRECAUCION', 2: 'OCUPADO'}
         return estados.get(valor, 'DESCONOCIDO')
     except:
         return None
 
+velocidad = 0
+
 while True:
-    velocidad_actual = random.randint(0, 150)
+    
+    if sp.is_connected():
+        data = sp.read()
+        if data:
+            cmd = data.decode().strip()
+            if cmd == "STOP":
+                velocidad = 0
+            elif cmd.isdigit():
+                velocidad = int(cmd)
+    else:
+        velocidad = random.randint(0, 150)
+
     estado = recibir_estado()
-
     if estado == 'OCUPADO':
-        velocidad_actual = 0
-    elif estado == 'PRECAUCION' and 110 <= velocidad_actual <= 120:
-        velocidad_actual = 50
-        
+        velocidad = 0
+    elif estado == 'PRECAUCION' and velocidad > 80:
+        velocidad = 50
 
-    velocidad_bits = min(int(velocidad_actual / 8), 15)
-    byte = (velocidad_bits << 4) | ID_LOCOMOTORA
-    tx_locomotora.send_byte(byte)
+    set_motor_speed(velocidad)
 
-    print(f"{NOMBRE_LOCOMOTORA} , Estado recibido: {estado}, Velocidad: {velocidad_actual} km/h, Byte enviado: {byte}")
-    time.sleep(2)
 
